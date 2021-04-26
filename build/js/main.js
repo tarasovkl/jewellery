71eb6edefe654dac199f93ab329c57c063322539
tarasovkl <tarasovk1991@gmail.com>
2021-04-26 16:27:20 +0300

    pp desktop

--- build/js/main.js
+++ build/js/main.js
@@ -2,9 +2,17 @@
 
 (function () {
   var questionList = document.querySelector(".questions__list");
-  var questionButtons = questionList.querySelectorAll(".questions__button");
-  var questionTitles = questionList.querySelectorAll(".questions__list-item h4");
-  var questionTexts = questionList.querySelectorAll(".questions__list-item p");
+  var questionButtons = document.querySelectorAll(".questions__button");
+  var questionTitles = document.querySelectorAll(".questions__list-item h4");
+  var questionTexts = document.querySelectorAll(".questions__list-item p");
+  var filter = document.querySelector(".filter");
+  var filterOptions = document.querySelectorAll(".filter__options");
+  var filterButtons = document.querySelectorAll(".filter__button");
+  var filterTitles = document.querySelectorAll(".filter__title p");
+  console.log(filterTitles);
+  console.log(filter);
+  console.log(filterButtons);
+  console.log(filterOptions);
 
   var toggleClass = function (element, selector) {
     element.classList.toggle(selector);
@@ -23,6 +31,22 @@
       button.addEventListener("click", function () {
         toggleClass(button, "questions__button--active");
         toggleClass(questionTexts[i], "questions__list-text");
+      });
+    });
+  }
+
+  if (filter) {
+    filterTitles.forEach(function (title, i) {
+      title.addEventListener("click", function () {
+        toggleClass(filterOptions[i], "filter__options--active");
+        toggleClass(filterButtons[i], "filter__button--active");
+      });
+    });
+
+    filterButtons.forEach(function (button, i) {
+      button.addEventListener("click", function () {
+        toggleClass(button, "filter__button--active");
+        toggleClass(filterOptions[i], "filter__options--active");
       });
     });
   }
